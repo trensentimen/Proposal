c63d110d7ad380b18d615ee17dca9f8c3777ac0d
Dani Ferdinan <daniferdinandall@gmail.com>
2023-10-08 11:05:41 +0700

    BERES

--- js/insert.js
+++ js/insert.js
@@ -1,101 +1,109 @@
 
-import data from "../json/porposal.json" assert { type: "json" };
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-// console.log(`${data.article.judulProposal}`)
-setInner("judulProposal", data.article.judulProposal);
+import { get } from "https://jscroot.github.io/api/croot.js";
 
-//Base
-setInner('basetitle', data.home.title);
-setInner('basedesc', data.home.description);
+// import data from "../json/porposal.json" assert { type: "json" };
 
-//Cover
-setInner("judulCover", data.cover.judulCover);
-setInner("proyek", data.cover.proyekProposal);
-setInner("tujuan", `
-<br> ${data.cover.tujuan[0]}
-<br> ${data.cover.tujuan[1]}
-<br> ${data.cover.tujuan[2]}
-`);
-setInner("namateam",`
-<p>${data.team[0].name}</p>
-<p>${data.team[1].name}</p>
-`)
-setInner("prodi", data.cover.prodi);
-setInner("univ", data.cover.univ);
-setInner("kota", data.cover.kota);
-setInner("tahun", data.cover.tahun);
 
-//Abstract
-setInner("abstract",`
-<h2>Abstract</h2>
-<p>${data.article.abstract[0]}</p>
-<p>${data.article.abstract[1]}</p>
-<p>${data.article.abstract[2]}</p>
-`);
+const responsFungsi = (data) => {
+    setInner("judulProposal", data.article.judulProposal);
 
-//Pengantar
-setInner("pengantarProposal", `
-<h2>Pengantar</h2>
-<p>${data.article.pengantarProposal}</p>
-`);
+    //Base
+    setInner('basetitle', data.home.title);
+    setInner('basedesc', data.home.description);
 
-//Latar Belakang
-let listLatarBelakang = data.article.latarBelakang4.map((item, index) => {
-    return `<li>${item}</li>`;
-});
-setInner("latarBelakangProposal", `
-<h2>Latar Belakang</h2>
-<p>${data.article.latarBelakang1}</p>
-<p>${data.article.latarBelakang2}</p>
-<p>${data.article.latarBelakang3}</p>
-<ul>${listLatarBelakang.join("")}</ul>
-<p>${data.article.latarBelakang5}</p>
-`);
+    //Cover
+    setInner("judulCover", data.cover.judulCover);
+    setInner("proyek", data.cover.proyekProposal);
+    setInner("tujuan", `
+        <br> ${data.cover.tujuan[0]}
+        <br> ${data.cover.tujuan[1]}
+        <br> ${data.cover.tujuan[2]}
+    `);
+    setInner("namateam", `
+        <p>${data.team[0].name}</p>
+        <p>${data.team[1].name}</p>
+    `)
+    setInner("prodi", data.cover.prodi);
+    setInner("univ", data.cover.univ);
+    setInner("kota", data.cover.kota);
+    setInner("tahun", data.cover.tahun);
 
-//Deskripsi Website
-let listDeskripsiWebsite = data.article.deskripsiWebsite.map((item, index) => {
-    return `<li>${item}</li>`;
-})
-setInner("deskripsiWebsite", `
-<h2>Deskripsi Website</h2>
-<ul>${listDeskripsiWebsite.join("")}</ul>
-`);
+    //Abstract
+    setInner("abstract", `
+    <h2>Abstract</h2>
+        <p>${data.article.abstract[0]}</p>
+        <p>${data.article.abstract[1]}</p>
+        <p>${data.article.abstract[2]}</p>
+    `);
 
-//DesaindanFungsionalitas
-let listDesaindanFungsionalitas = data.article.DesaindanFungsionalitasList.map((item, index) => {
-    return `<li>${item}</li>`;
-})
-setInner("desaindanFungsionalitas", `
-<h2>Desain dan Fungsionalitas</h2>
-<ul>${listDesaindanFungsionalitas.join("")}</ul>
-`);
+    //Pengantar
+    setInner("pengantarProposal", `
+        <h2>Pengantar</h2>
+        <p>${data.article.pengantarProposal}</p>
+    `);
 
-//Pemasaran
-setInner("pemasaran", `
-<h2>Pemasaran</h2>
-<p>${data.article.Pemasaran}</p>
-<p>${data.article.Pemasaran2}</p>
-<p>${data.article.Pemasaran3}</p>
-`);
+    //Latar Belakang
+    let listLatarBelakang = data.article.latarBelakang4.map((item, index) => {
+        return `<li>${item}</li>`;
+    });
+    setInner("latarBelakangProposal", `
+        <h2>Latar Belakang</h2>
+        <p>${data.article.latarBelakang1}</p>
+        <p>${data.article.latarBelakang2}</p>
+        <p>${data.article.latarBelakang3}</p>
+        <ul>${listLatarBelakang.join("")}</ul>
+        <p>${data.article.latarBelakang5}</p>
+    `);
 
-//waktuPelaksanaan
-setInner("waktuPelaksanaan", `
-<h2>Waktu Pengerjaan</h2>
-<p>${data.article.waktuPelaksanaan}</p>
-`);
+    //Deskripsi Website
+    let listDeskripsiWebsite = data.article.deskripsiWebsite.map((item, index) => {
+        return `<li>${item}</li>`;
+    })
+    setInner("deskripsiWebsite", `
+        <h2>Deskripsi Website</h2>
+        <ul>${listDeskripsiWebsite.join("")}</ul>
+    `);
 
-//Kesimpulan
-setInner("kesimpulan", `
-<h2>Kesimpulan</h2>
-<p>${data.article.kesimpulan}</p>
-<p>${data.article.kesimpulan2}</p>
-<p>${data.article.kesimpulan3}</p>
-`);
+    //DesaindanFungsionalitas
+    let listDesaindanFungsionalitas = data.article.DesaindanFungsionalitasList.map((item, index) => {
+        return `<li>${item}</li>`;
+    })
+    setInner("desaindanFungsionalitas", `
+        <h2>Desain dan Fungsionalitas</h2>
+        <ul>${listDesaindanFungsionalitas.join("")}</ul>
+    `);
 
-//Team
-setInner("name2", data.team[0].name);
-setInner("email2", data.team[0].email);
-setInner("npm2", data.team[0].npm);
-setInner("name1", data.team[1].name);
-setInner("email1", data.team[1].email);
-setInner("npm1", data.team[1].npm);
+    //Pemasaran
+    setInner("pemasaran", `
+        <h2>Pemasaran</h2>
+        <p>${data.article.Pemasaran}</p>
+        <p>${data.article.Pemasaran2}</p>
+        <p>${data.article.Pemasaran3}</p>
+    `);
+
+    //waktuPelaksanaan
+    setInner("waktuPelaksanaan", `
+        <h2>Waktu Pengerjaan</h2>
+        <p>${data.article.waktuPelaksanaan}</p>
+    `);
+
+    //Kesimpulan
+    setInner("kesimpulan", `
+        <h2>Kesimpulan</h2>
+        <p>${data.article.kesimpulan}</p>
+        <p>${data.article.kesimpulan2}</p>
+        <p>${data.article.kesimpulan3}</p>
+    `);
+
+    //Team
+    setInner("name2", data.team[0].name);
+    setInner("email2", data.team[0].email);
+    setInner("npm2", data.team[0].npm);
+    setInner("name1", data.team[1].name);
+    setInner("email1", data.team[1].email);
+    setInner("npm1", data.team[1].npm);
+};
+
+await get("https://raw.githubusercontent.com/trensentimen/trensentimen.github.io/main/json/porposal.json", responsFungsi);
+
